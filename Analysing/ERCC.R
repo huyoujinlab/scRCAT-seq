b434db652e9c43a8cb444a47eb0abb8d86e5e91f
huyoujinlab <54207658+huyoujinlab@users.noreply.github.com>
2020-01-03 21:22:07 +0800

    Update ERCC.R

--- Analysing/ERCC.R
+++ Analysing/ERCC.R
@@ -60,3 +60,36 @@ ggplot(df, aes(x=Mix1, y=tss_rpm1))+
               se = F, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE,color="red")+
   scale_x_continuous(breaks = seq(0,4,1)) +
   scale_y_continuous(limits = c(0,6),breaks = seq(0,6,1)) 
+
+
+
+
+
+
+df <- merge(concentration[,c(1,3)],tes_rpm_new,by = "ERCC",all = T)   ########tes
+df[is.na(df)] <- 0
+
+df[,2] <- log10(df[,2]+1)
+df[,3] <- log10(df[,3]+1)
+
+
+
+
+cor(df[,2:3],method = "spearman")
+ggplot(df, aes(x=Mix1, y=tes_rpm1))+
+  geom_point(size=0.9,color="green")+
+  labs(x="log10(ERCC+1)",y="Obversed level")+
+  theme_set(theme_bw()) +
+  theme(panel.grid.major=element_line(colour=NA),panel.grid.minor=element_line(colour=NA))+
+  theme(panel.border = element_blank())+ 
+  theme(axis.line.x=element_line(linetype=1,color="black",size=1), 
+        axis.line.y=element_line(linetype=1,color="black",size=1), 
+        axis.text.x = element_text(face = "plain",size = 10.5), 
+        axis.text.y = element_text(face = "plain",size = 10.5), 
+        legend.position="none") +
+  geom_smooth(mapping = NULL, data = NULL, stat = "smooth",
+              position = "identity", method = "lm", formula = y ~ x,
+              se = F, na.rm = FALSE, show.legend = NA, inherit.aes = TRUE,color="red")+
+  scale_x_continuous(breaks = seq(0,4,1)) +
+  scale_y_continuous(limits = c(0,6),breaks = seq(0,6,1)) 
+
